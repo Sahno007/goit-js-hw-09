e32ab7eb80223144f6e2e00c05a3673439acef8c
Sahno007 <129589936+Sahno007@users.noreply.github.com>
2023-08-17 14:45:22 +0000

    Deploying to gh-pages from @ Sahno007/goit-js-hw-09@d4a130c791979373ac7c625e0964f383825e993b 🚀

---
+++ 01-color-switcher.b3c07bdf.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null,a=function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))};t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval(a,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n),n=null}))}();
+//# sourceMappingURL=01-color-switcher.b3c07bdf.js.map
