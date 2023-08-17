7b2c29d56a198affd4935de39f2a96f50497ccaf
Sahno007 <turnskin90@gmail.com>
2023-08-17 19:20:20 +0300

    upg

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,9 @@
-import Notiflix from "notiflix"; // Переконайтеся, що бібліотека notiflix підключена до вашого проекту
+import Notiflix from 'notiflix';
 
-// Отримання полів форми
 const form = document.querySelector('.form');
-const delayInput = form.querySelector('input[name="delay"]');
-const stepInput = form.querySelector('input[name="step"]');
-const amountInput = form.querySelector('input[name="amount"]');
-const submitButton = form.querySelector('button[type="submit"]');
 
-// Функція для створення промісу з відповідним виконанням чи відхиленням
+form.addEventListener('submit', handleSubmit);
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
@@ -22,42 +18,26 @@ function createPromise(position, delay) {
   });
 }
 
-// Обробник події відправки форми
-form.addEventListener('submit', async (event) => {
+function handleSubmit(event) {
   event.preventDefault();
 
-  const initialDelay = parseInt(delayInput.value);
-  const step = parseInt(stepInput.value);
-  const amount = parseInt(amountInput.value);
+  const delayInput = document.querySelector('[name="delay"]');
+  const stepInput = document.querySelector('[name="step"]');
+  const amountInput = document.querySelector('[name="amount"]');
 
-  submitButton.disabled = true;
+  const initialDelay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+  const amount = Number(amountInput.value);
 
   for (let i = 1; i <= amount; i++) {
-    try {
-      await createPromise(i, initialDelay + (i - 1) * step);
-      Notiflix.Notify.Success(`✅ Fulfilled promise ${i}`);
-    } catch (error) {
-      Notiflix.Notify.Failure(`❌ Rejected promise ${error.position}`);
-    }
+    const currentDelay = initialDelay + (i - 1) * step;
+
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.Success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.Failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
-
-  submitButton.disabled = false;
-});
-
-// Ініціалізація flatpickr
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    if (selectedDates[0] < new Date()) {
-      Notiflix.Notify.Warning("Please choose a date in the future");
-      submitButton.disabled = true;
-    } else {
-      submitButton.disabled = false;
-    }
-  },
-};
-
-flatpickr("#datetime-picker", options);
+}
